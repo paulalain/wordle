12a9d4d8b6308c3e234acfc70a6603799f122364
Paul-Alain Bugnard <paul.alain.bugnard@gmail.com>
2025-04-06 08:42:40 +0200

    Update GameOver.js

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -17,7 +17,7 @@ function GameOver() {
       )}
       {gameOver.guessedWord && (
         <p>
-          <h1>Bravo vous avez trouvé le bon prénom !</h1>
+          <h1>Bravo vous avez trouvé son prénom !</h1>
           <h2>Jacob - 6 avril 2025 à 05:37</h2>
           <img src={babyPicture} class="baby" /><br /><br />
           Si vous souhaitez participer voici notre <a href="https://www.majolieliste.fr/liste/449634">liste de naissance</a>.
